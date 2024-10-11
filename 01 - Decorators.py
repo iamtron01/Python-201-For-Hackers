33fc752e1929134d9fc10f867a248d304419921f
Michael Byrd <iamtron01@hotmail.com>
2024-10-11 12:10:11 -0500

    Added

---
+++ 01 - Decorators.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+import time
+
+def logger(func):
+    def wrapper():
+        print("-"*50)
+        print("> Execution started {}"
+              .format(datetime.today()
+              .strftime("%Y-%m-%d %H:%M:%S")))
+        func()
+        print("> Execution completed {}"
+              .format(datetime.today()
+              .strftime("%Y-%m-%d %H:%M:%S")))
+        print("-"*50)
+    return wrapper
+
+@logger
+def demo_function():
+    print("Executing task!")
+    time.sleep(3)
+    print("Task Completed!")
+
+demo_function()
+
+def logger_args(func):
+    def wrapper(*args, **kwargs):
+        print("-"*50)
+        print("> Execution started {}"
+                .format(datetime.today()
+                .strftime("%Y-%m-%d %H:%M:%S")))
+        func(*args, **kwargs)
+        print("> Execution completed {}"
+                .format(datetime.today()
+                .strftime("%Y-%m-%d %H:%M:%S")))
+        print("-"*50)
+    return wrapper
+
+@logger_args
+def demo_function_args(sleep_time):
+    print("Executing task!")
+    time.sleep(sleep_time)
+    print("Task complete!")
+
+demo_function_args(1)
